5819fb32208712d43ca607b63b613faf18ec7544
brady bridges <bradyjbridges@gmail.com>
2026-04-28 22:15:47 -0500

    wip: project showcase section

---
+++ src/routes/ProjectCards.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { projects } from './projects';
+</script>
+
+{#each projects as project}
+	<div class="project-card flex flex-col overflow-hidden rounded-xl bg-gray-900">
+		<a href={project.url} class="mb-2 border-b border-b-cyan-400 pb-4" target="_blank">
+			<img class="h-48 w-full object-cover object-bottom" src={project.image} alt="EasyRef" />
+		</a>
+		<div class="flex flex-col items-start gap-3 px-2 py-3">
+			<div>
+				<h3 class="mb-1 text-lg">{project.name}</h3>
+				<p class="text-xs">{project.description}</p>
+			</div>
+			<div>
+				<p class="mb-2 text-xs">Built With</p>
+				<div class="flex flex-wrap gap-2">
+					{#each project.badges as badge}
+						<span class="rounded-full border border-gray-500 bg-gray-800 px-3 text-xs"
+							>{badge}</span
+						>
+					{/each}
+				</div>
+			</div>
+		</div>
+	</div>
+{/each}
+
+<style>
+	.project-card {
+		flex-basis: calc(25% - 12px);
+	}
+</style>
