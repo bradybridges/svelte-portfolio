afc90e8e6e7ad92b9cdfc18821f21c7330f49d65
brady bridges <bradyjbridges@gmail.com>
2026-04-28 22:15:27 -0500

    wip: hero section

---
+++ src/routes/HeroSection.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import headshot from '$lib/assets/Brady Bridges.jpg';
+</script>
+
+<section class="my-6 px-12 py-4">
+	<div class="hero-container gap-4">
+		<div class="overflow-hidden rounded-xl">
+			<img src={headshot} alt="headshot" />
+		</div>
+
+		<div class="flex w-full flex-col items-center justify-center gap-2">
+			<h2 class="text-3xl">Hero Section</h2>
+			<p class="p-4">adfasd sf asdf dfa sdf sdfasd f</p>
+		</div>
+	</div>
+</section>
+
+<style>
+	.hero-container {
+		display: grid;
+		grid-template-columns: 1fr 2fr;
+	}
+</style>
