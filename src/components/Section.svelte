6db45071a6cbb2ef3a26a183fdd0d09a7b878d91
brady bridges <bradyjbridges@gmail.com>
2026-05-03 19:23:06 -0500

    ui: add section to make container spacing consistent/controllable

---
+++ src/components/Section.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	let { children, ...attributes }: SvelteHTMLElements['section'] = $props();
+</script>
+
+<section class={['mt-6 mb-12 md:mb-24 px-4 md:px-12 lg:px-24 py-4', attributes.class]}>
+	{@render children?.()}
+</section>
