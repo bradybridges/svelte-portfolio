9edb86cfe4cfea12da89b8221340c9ae560c32a2
brady bridges <bradyjbridges@gmail.com>
2026-05-03 20:05:31 -0500

    ui: add employment status component

---
+++ src/components/StatusSection.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Section from "./Section.svelte";
+</script>
+
+<Section class="mt-12 md:mb-12">
+	<div class="flex flex-col gap-8 rounded-lg border border-emerald-400/50 bg-gray-900 p-8 shadow-lg shadow-cyan-400/33 lg:shadow-2xl">
+		<div class="flex flex-col items-start justify-center gap-6">
+			<h2 class="text-3xl">Status</h2>
+
+			<div class="text-lg">
+				Recruitment Status:
+				<span class="ml-2 inline-block pb-1 font-bold text-emerald-400">Open To Work</span>
+			</div>
+		</div>
+	</div>
+</Section>
