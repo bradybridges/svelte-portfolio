1eefb7a1c620cbcd3e4ca53d067153b543ba0853
brady bridges <bradyjbridges@gmail.com>
2026-05-06 12:39:13 -0500

    chore: add support for target="_blank" attribute

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -4,14 +4,16 @@
 		label: string;
 		classes?: string;
 		ariaLabel?: string;
+		target?: '_blank' | false;
 	}
-	let { url, label, ariaLabel, classes = '' }: Props = $props();
+	let { url, label, ariaLabel, classes = '', target = false }: Props = $props();
 </script>
 
 <a
 	href={url}
 	class={['transition-colors hover:text-cyan-400', classes]}
 	aria-label={ariaLabel ? ariaLabel : null}
+	target={target ? target : null}
 >
 	{label}
 </a>
