0a355f65e9d25e4d52af6217939696d39eb9f876
brady bridges <bradyjbridges@gmail.com>
2026-05-20 20:54:56 -0500

    feat: add support for displaying icon via prop

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
+	import Icon from "@iconify/svelte";
+
 	interface Props {
 		url: string;
 		label: string;
 		classes?: string;
 		ariaLabel?: string;
 		target?: '_blank' | false;
+		showIcon?: boolean;
 	}
-	let { url, label, ariaLabel, classes = '', target = false }: Props = $props();
+	let { url, label, ariaLabel, classes = '', target = false, showIcon = false }: Props = $props();
 </script>
 
 <a
 	href={url}
-	class={['transition-colors hover:text-cyan-400', classes]}
+	class={['transition-colors hover:text-cyan-400', showIcon && 'flex gap-2 items-center', classes]}
 	aria-label={ariaLabel ? ariaLabel : null}
 	target={target ? target : null}
 >
 	{label}
+
+	{#if showIcon}
+		<Icon icon="line-md:link" height="16" />
+	{/if}
 </a>
